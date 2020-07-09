b392cde3cc58b74647864cf22af30ce171a805e0
Andras Torok <>
2020-07-09 15:16:42 +0200

    Added todo-services module with Todo and Priority classes.

---
+++ todo-services/src/test/java/com/todoapp/services/todo/todoservices/TodoServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoapp.services.todo.todoservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
