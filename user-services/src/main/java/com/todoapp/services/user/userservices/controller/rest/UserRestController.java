347e64de432b7bf23b8817a5b6418759002a06c0
Andras Torok <>
2020-07-20 15:13:33 +0200

    Done with API Gateway

--- user-services/src/main/java/com/todoapp/services/user/userservices/controller/rest/UserRestController.java
+++ user-services/src/main/java/com/todoapp/services/user/userservices/controller/rest/UserRestController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 import static com.todoapp.services.user.userservices.util.Constants.REQUEST_MAPPING_USERS;
 
 @RestController
+@RequestMapping("/user")
 public class UserRestController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserRestController.class);
